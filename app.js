832bc7e3eabcba4d64c79547832931eb9852039d
springlucky <dlchunl@cn.ibm.com>
2016-11-19 14:43:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ io.on("connection",function(socket){
 			//************************
 			//fill in a method "io.sockets.emit(param1,param2)" in line 35
 			//The first parameter is the data type "message", the second parameter is the data you want to send
-					
+			io.sockets.emit("message", key);		
 		}
 		else{
 			console.log("Not a direction order");
